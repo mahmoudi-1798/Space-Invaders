3a7d8e18325fd20662bcc17c95a12db8ca93230b
kourosh <kourosh.mahmoudi.2004@gmail.com>
2023-06-25 13:02:44 +0330

    adding menu cursor

--- main.cpp
+++ main.cpp
@@ -38,6 +38,7 @@ Interval* addedUnderLineTearInterval;
 
 App app;
 Entity player;
+Entity cursor;
 
 void renderMenu(SDL_Renderer* renderer, TTF_Font* font);
 void displayMenu(SDL_Window* window, SDL_Renderer* renderer, bool &game, bool &menu, bool& setting, bool& about, bool &fullscreen,int &menuOption, SDL_Event e);
@@ -216,7 +217,6 @@ int main() {
         memset(&player, 0, sizeof(Entity));
 
         player.texture = loadTexture(loadSurface("resources/playership.png"));
-        //SDL_Texture* playBackground = loadTexture(loadSurface("resources/background1.jpg"));
         enemyBulletTexture = loadTexture(loadSurface("resources/enemyBullet.png"));
         extraEnemyTexture = loadTexture(loadSurface("resources/extraEnemy.png"));
         enemyFirstRowBTexture = loadTexture(loadSurface("resources/1b.png"));
@@ -262,7 +262,6 @@ int main() {
 
             start = clock();
             prepareScene();
-            // blit(playBackground, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
             doInput(&requestClose);
 
@@ -371,25 +370,24 @@ int main() {
     return 0;
 }
 
-void renderMenu(SDL_Renderer* renderer, TTF_Font* font1, TTF_Font* font2) {
+void renderMenu(SDL_Renderer* renderer, TTF_Font* font1, TTF_Font* font2, int c_x, int c_y) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_Surface* cursor = IMG_Load("resources/cursor.png");
-    SDL_Texture* cursorTex = SDL_CreateTextureFromSurface(renderer, cursor);
-    SDL_Rect curRect = {SCREEN_WIDTH / 2 - 180, SCREEN_HEIGHT/2 + 120, 17, 19};
-    SDL_RenderCopy(renderer, cursorTex, NULL, &curRect);
-
-
     SDL_Color textColor = { 255, 255, 255 };
-    //SDL_Surface* textSurface1 = TTF_RenderText_Solid(font2, "Space Invaders", textColor);
-    //SDL_Texture* textTexture1 = SDL_CreateTextureFromSurface(renderer, textSurface1);
 
     SDL_Surface* logo = IMG_Load("resources/logo.png");
     SDL_Texture* logoTex = SDL_CreateTextureFromSurface(renderer, logo);
     SDL_Rect logoRect = {185, 30, 900, 389};
     SDL_RenderCopy(renderer, logoTex, NULL, &logoRect);
 
+    memset(&cursor, 0, sizeof(Entity));
+    cursor.texture = loadTexture(loadSurface("resources/cursor.png"));
+    cursor.x = c_x;
+    cursor.y = c_y;
+    cursor.width = 17;
+    cursor.height = 19;
+    blit(cursor.texture, cursor.x, cursor.y, cursor.width, cursor.height);
 
     SDL_Surface* textSurface2 = TTF_RenderText_Solid(font1, "New Game", textColor);
     SDL_Texture* textTexture2 = SDL_CreateTextureFromSurface(renderer, textSurface2);
@@ -402,10 +400,6 @@ void renderMenu(SDL_Renderer* renderer, TTF_Font* font1, TTF_Font* font2) {
 
     SDL_Surface* textSurface5 = TTF_RenderText_Solid(font1, "Exit", textColor);
     SDL_Texture* textTexture5 = SDL_CreateTextureFromSurface(renderer, textSurface5);
-    
-    //SDL_Rect textRect1 = { SCREEN_WIDTH / 2 - textSurface1->w / 2, SCREEN_HEIGHT / 3 - textSurface1->h / 2,
-      //                    textSurface1->w, textSurface1->h };
-    //SDL_RenderCopy(renderer, textTexture1, NULL, &textRect1);
 
      
     SDL_Rect textRect2 = { SCREEN_WIDTH / 2 - textSurface2->w / 2, (SCREEN_HEIGHT / 2 - textSurface2->h / 2) + 140,
@@ -424,11 +418,7 @@ void renderMenu(SDL_Renderer* renderer, TTF_Font* font1, TTF_Font* font2) {
                           textSurface5->w, textSurface5->h };
     SDL_RenderCopy(renderer, textTexture5, NULL, &textRect5);
 
-    SDL_DestroyTexture(cursorTex);
-    SDL_FreeSurface(cursor);
-
-    //SDL_DestroyTexture(textTexture1);
-    //SDL_FreeSurface(textSurface1);
+    SDL_DestroyTexture(cursor.texture);
 
     SDL_DestroyTexture(logoTex);
     SDL_FreeSurface(logo);
@@ -453,6 +443,9 @@ void displayMenu(SDL_Window* window, SDL_Renderer* renderer, bool &game, bool &m
     TTF_Font* SansBold = TTF_OpenFont("resources/EnvyCode.ttf", BOLD_SANS);
     TTF_Font* SansBig = TTF_OpenFont("resources/EnvyCode.ttf", BIG_SANS);
     
+    cursor.x = SCREEN_WIDTH / 2 - 120;
+    cursor.y = SCREEN_HEIGHT/2 + 130;
+
     while(menu){
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
@@ -461,11 +454,17 @@ void displayMenu(SDL_Window* window, SDL_Renderer* renderer, bool &game, bool &m
             else if (e.type == SDL_KEYDOWN) {
                 if (e.key.keysym.sym == SDLK_UP) {
                     // Handle up arrow key press to move the selection up
-                    menuOption = (menuOption - 1 + 4) % 4; // Assuming you have 3 menu options
+                    menuOption = (menuOption - 1 + 4) % 4; 
+                    if (menuOption == 3) {cursor.y = SCREEN_HEIGHT/2 + 280;}
+                    else {cursor.y -= 50;}
+                    
                 }
                 else if (e.key.keysym.sym == SDLK_DOWN) {
                     // Handle down arrow key press to move the selection down
-                    menuOption = (menuOption + 1) % 4; // Assuming you have 3 menu options
+                    menuOption = (menuOption + 1) % 4;
+                    if (menuOption == 0) {cursor.y = SCREEN_HEIGHT/2 + 130;}
+                    else {cursor.y += 50;}
+                    
                 }
                 else if (e.key.keysym.sym == SDLK_RETURN) {
                     // Handle enter key press to select an option
@@ -477,19 +476,18 @@ void displayMenu(SDL_Window* window, SDL_Renderer* renderer, bool &game, bool &m
                             break;
                         case 1:
                             // Options
-                            // Add your options logic here
                             setting = true;
                             displaySetting(renderer, setting, menu, e);
                             break;
                         case 2:
-                            // Exit game
+                            // About game
                             about = true;
                             displayAbout(renderer, about, menu, e);
                             break;
 
                         case 3:
                             // Exit game
-                            menu = false; // Exit the program
+                            menu = false; 
                             game = false;
                             break;
                     }
@@ -500,11 +498,12 @@ void displayMenu(SDL_Window* window, SDL_Renderer* renderer, bool &game, bool &m
                     }
                     else{
                         SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+                        SDL_SetWindowSize(window, SCREEN_WIDTH, SCREEN_HEIGHT);
                     }  
                 }
             }
         }
-        renderMenu(renderer, SansBold, SansBig);
+        renderMenu(renderer, SansBold, SansBig, cursor.x, cursor.y);
     }
 }
 
@@ -606,7 +605,7 @@ void renderAbout(SDL_Renderer* renderer, TTF_Font* font1, TTF_Font* font2){
     SDL_Surface* textSurface1 = TTF_RenderText_Solid(font2, "About", textColor);
     SDL_Texture* textTexture1 = SDL_CreateTextureFromSurface(renderer, textSurface1);
 
-    SDL_Surface* textSurface2 = TTF_RenderText_Solid(font1, "Final project of Advanced Progrmming (Spring Semestre)", textColor);
+    SDL_Surface* textSurface2 = TTF_RenderText_Solid(font1, "Final project of Advanced Progrmming (Spring Semester)", textColor);
     SDL_Texture* textTexture2 = SDL_CreateTextureFromSurface(renderer, textSurface2);
     
     SDL_Surface* textSurface11 = TTF_RenderText_Solid(font2, "Socials", textColor);
